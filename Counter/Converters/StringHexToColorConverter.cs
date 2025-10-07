c3cf9b5ee76f7fa20cb09f2cbe6b93532e3cf995
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-10-07 10:11:13 +0200

    feat: Add color personalization for counters

---
+++ Counter/Converters/StringHexToColorConverter.cs
@@ -0,0 +1,23 @@
+
+using System.Globalization;
+using Microsoft.Maui.Graphics;
+
+namespace Counter.Converters
+{
+    public class StringHexToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string hexCode)
+            {
+                return Color.FromArgb(hexCode);
+            }
+            return Colors.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
