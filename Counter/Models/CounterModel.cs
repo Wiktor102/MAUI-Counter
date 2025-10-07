6eed7c06b55cf35b1f8c335a208f7ff2cf19f51a
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-10-07 10:03:11 +0200

    feat: Initial project structure for MVVM

---
+++ Counter/Models/CounterModel.cs
@@ -0,0 +1,72 @@
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Counter.Models
+{
+    public class CounterModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private int _value;
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private int _initialValue;
+        public int InitialValue
+        {
+            get => _initialValue;
+            set
+            {
+                if (_initialValue != value)
+                {
+                    _initialValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _color;
+        public string Color
+        {
+            get => _color;
+            set
+            {
+                if (_color != value)
+                {
+                    _color = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Counter/Views/CountersView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Counter.Views;
+
+public partial class CountersView : ContentPage
+{
+	public CountersView()
+	{
+		InitializeComponent();
+	}
+}
